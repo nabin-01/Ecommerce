053959c711c02d25dff5e33269023bb1ab378207
nabin-01 <nabinghimire1001@gmail.com>
2021-04-06 21:18:09 +0545

    Merge branch 'master'

--- home/views.py
+++ home/views.py
@@ -167,7 +167,3 @@ def signup(request):
             messages.error(request, 'These passwords do not match!')
             return redirect('home:account')
     return render(request, 'signup.html')
-
-
-# def login(request):
-#     if request.method == 'POST':
